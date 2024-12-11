2bf1b32f627fb2c8244448661644ae2e5a677581
lud <114997596+ludovic4g@users.noreply.github.com>
2024-12-11 12:54:49 +0100

    Added comments

--- src/postbot/scripts/init_system.py
+++ src/postbot/scripts/init_system.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import rospy
 import random
+from geometry_msgs.msg import Pose
 from postbot.msg import BoxInfo
 from postbot.srv import reset_boxes, reset_boxesResponse
 from visualization_msgs.msg import Marker, MarkerArray
@@ -11,35 +12,47 @@ robotpub = None
 def spawn_robot():
     global robotpub
     robot_name = "postbot"
+    # Correzione del parametro: robot_initial_pose (prima era robor_initial_pose)
+    robot_initial_pose = rospy.get_param('robot_initial_pose', 'src/postbot/config/robot_par.yaml')
 
-    init_pose = rospy.get_param('robot_initial_pose', {'x':0.5, 'y':0.5, 'z':0.25})
+    model_path = rospy.get_param('model_path', '/home/ludoludo/postbot/src/postbot/urdf/postbot.urdf')
 
+    with open(model_path, 'r') as urdf_file:
+        robot_urdf = urdf_file.read()
+        rospy.loginfo("Urdf Model read")
+
+    rospy.loginfo("Spawned in Gazebo")
 
     marker = Marker()
+    # Cambiato frame_id da "world" a "map"
     marker.header.frame_id = "map"
     marker.id = 0
     marker.type = Marker.CUBE
     marker.action = Marker.ADD
-    marker.pose.position.x = init_pose['x']
-    marker.pose.position.y = init_pose['y']
-    marker.pose.position.z = init_pose['z']
+    marker.pose.position.x = 0.5
+    marker.pose.position.y = 0.5
+    marker.pose.position.z = 0.25
     marker.scale.x = 0.5
     marker.scale.y = 0.5
     marker.scale.z = 0.5
+    # cambia colore
     marker.color.r = 0.0
     marker.color.g = 1.0
     marker.color.b = 0.0
     marker.color.a = 1.0
     robotpub.publish(marker)
 
-    rospy.loginfo("Spawned Robot Marker in RViz")
+    rospy.loginfo("Spawned in Rviz and published")
+
 
 def spawn_boxes(box):
     global boxpub
+    boxpub = rospy.Publisher('/box_marker',MarkerArray, queue_size=10)
     boxes = MarkerArray()
 
     for i, (x, y, color) in enumerate(zip(box.x, box.y, box.colors)):
         marker = Marker()
+        # Cambiato frame_id da "world" a "map"
         marker.header.frame_id = "map"
         marker.ns = "boxes"
         marker.id = i
@@ -53,18 +66,35 @@ def spawn_boxes(box):
         marker.scale.z = 0.5
 
         if color == 'red':
-            marker.color.r = 1.0; marker.color.g = 0.0; marker.color.b = 0.0
-        elif color == 'blue':
-            marker.color.r = 0.0; marker.color.g = 0.0; marker.color.b = 1.0
-        elif color == 'green':
-            marker.color.r = 0.0; marker.color.g = 1.0; marker.color.b = 0.0
-        elif color == 'white':
-            marker.color.r = 1.0; marker.color.g = 1.0; marker.color.b = 1.0
-        elif color == 'purple':
-            marker.color.r = 0.5; marker.color.g = 0.0; marker.color.b = 0.5
-        elif color == 'yellow':
-            marker.color.r = 1.0; marker.color.g = 1.0; marker.color.b = 0.0
+            marker.color.r = 1.0
+            marker.color.g = 0.0
+            marker.color.b = 0.0
 
+        elif color == 'blue':
+            marker.color.r = 0.0
+            marker.color.g = 0.0
+            marker.color.b = 1.0
+        
+        elif color == 'green':
+            marker.color.r = 0.0
+            marker.color.g = 1.0
+            marker.color.b = 0.0
+        
+        elif color == 'white':
+            marker.color.r = 1.0
+            marker.color.g = 1.0
+            marker.color.b = 1.0
+
+        elif color == 'purple':
+            marker.color.r = 0.5
+            marker.color.g = 0.0
+            marker.color.b = 0.5
+
+        elif color == 'yellow':
+            marker.color.r = 1.0
+            marker.color.g = 1.0
+            marker.color.b = 0.0
+        
         marker.color.a = 1.0
         boxes.markers.append(marker)
     
@@ -74,20 +104,28 @@ def init_boxes():
     pub = rospy.Publisher('/box_status', BoxInfo, queue_size=10)
     rospy.sleep(5)
 
+    rospy.loginfo("Prima del while")
     box = BoxInfo()
     box.colors =  ['red', 'blue', 'green', 'yellow', 'white', 'purple']
-    # Posizioni iniziali di esempio, personalizzare se necessario
-    box.x = [1, 2, 3, 0, -1, -2]
-    box.y = [1, 1, 1, 1, 2, 3]
-    box.status = [0, 0, 0, 0, 0, 0]
+    box.x = [1, 2, 3, 0, 0 ,0]
+    box.y = [1, 1, 1, 1, 2 ,3]
+    box.status = [0, 0, 0, 0, 0 ,0]
+
+    rospy.loginfo(f"box value: {box}")
 
     spawn_boxes(box)
     spawn_robot()
 
+    #flag used : the topic is published only one time
+
     spawn = False
     while not rospy.is_shutdown():
         if not spawn:
+            # box_status is published only one time without rate
+            rospy.loginfo("Publishing into /box_status topic")
             pub.publish(box)
+            rospy.loginfo("Published into /box_status topic")
+        
             spawn = True
         else:
             break
@@ -97,12 +135,18 @@ def handle_reset_boxes(req):
     rospy.loginfo("Reset service call arrived, done = true")
     return reset_boxesResponse(done=True)
 
+
 if __name__ == "__main__":
+
     rospy.init_node("init_spawn", anonymous=False)
     rospy.loginfo("Initializing system")
+    # spawn_robot_boxes() 
     robotpub = rospy.Publisher('/robot_marker', Marker, queue_size=10)
     boxpub = rospy.Publisher('/box_marker', MarkerArray, queue_size=10)
+    
     rospy.Service('reset_boxes', reset_boxes, handle_reset_boxes)
     rospy.loginfo("Ready to reset boxes")
+
     init_boxes()
+
     rospy.spin()
--- src/postbot/scripts/managing_boxes.py
+++ src/postbot/scripts/managing_boxes.py
@@ -2,45 +2,66 @@
 import rospy
 from postbot.msg import BoxGoal, BoxInfo, MarbleInfo
 from threading import Condition
+from visualization_msgs.msg import Marker, MarkerArray
 
 flag = False
 box_flag = False
-sequencing = Condition()
+sequencing = Condition() # Thread for the right sequencing of callbacks
 rospy.init_node('managing_boxes', anonymous=False)
 
+rate = rospy.Rate(1)
+rospy.loginfo("Initializing node")
+new_box_color = ' '
 colors = ['red', 'blue', 'green', 'yellow', 'white', 'purple']
 box_goal = None
+box_status = None
 
-def marble_callback(data):
-    global flag, new_box_color
+def callback(data):
+    global new_box_color, flag
     with sequencing:
+        rospy.loginfo(f"Color of the marble : {data.color}")
         new_box_color = data.color
         flag = True
+        rospy.loginfo(f"Box assigned color 1: {new_box_color}")
         sequencing.notify_all()
 
 def boxstatus_callback(data):
-    global box_goal, box_flag, new_box_color
+    rospy.loginfo("Sono nel secondo callback")
+    global box_goal, box_flag, box_status
     with sequencing:
         while not flag:
             sequencing.wait()
-        color_position = colors.index(new_box_color)
+        color_position = colors.index(new_box_color) 
+        rospy.loginfo(f"color position? : {color_position}")
+        box_status = list(data.status)
         box_goal = BoxGoal()
         box_goal.x = data.x[color_position]
         box_goal.y = data.y[color_position]
         box_goal.color = colors[color_position]
-        box_flag = True
+        box_flag= True
+        rospy.loginfo(f"Box: {box_goal}")
         sequencing.notify_all()
 
-rospy.Subscriber('/current_marble', MarbleInfo, marble_callback)
+# Subscribes to the topic /current_marble to take the color and its position
+rospy.Subscriber('/current_marble', MarbleInfo, callback)
+
+# Subscribes to the topic /box_status to find box needing delivery
 rospy.Subscriber('/box_status', BoxInfo, boxstatus_callback)
 
+rospy.loginfo(f"Box assigned color 2: {new_box_color}")
+
+# Publish the coordinates of the box that has to be delivered
 pub = rospy.Publisher('/box_goal', BoxGoal, queue_size=10)
 
-rate = rospy.Rate(1)
+boxpub = rospy.Publisher('/box_marker', MarkerArray, queue_size=10)
 while not rospy.is_shutdown():
     with sequencing:
         if flag and box_flag:
+            rospy.loginfo(f"Box to deliver: {box_goal}")
+            rospy.loginfo(f"Publishing into /box_goal topic")
             pub.publish(box_goal)
+            rospy.loginfo(f"Published into /box_goal topic")
             flag = False
             box_flag = False
+
     rospy.sleep(5)
